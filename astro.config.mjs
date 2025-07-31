3229dbc5c9ef01a0b2ad7f63cad62df4d58b0d7c
Jacob Colburn <huntercolburn2197@icloud.com>
2025-07-31 17:16:11 -0500

    Uploading updated files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    server: {
+        port: 3000
+    },
+
+    devToolbar:{
+        enabled: false,
+    },
+});
